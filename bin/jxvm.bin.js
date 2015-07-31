8dd6085769229b45b4c7a6d8cb1c4c326634d6d0
ktrzeciaknubisa <ktrzeciak@nubisa.com>
2015-07-31 16:26:21 +0200

    updates

--- bin/jxvm.bin.js
+++ bin/jxvm.bin.js
@@ -2,4 +2,41 @@
 
 // Copyright & License details are available under JXCORE_LICENSE file
 
-console.log("Hello!");
+var logErrorAndExit = function() {
+  console.error.apply(null, arguments);
+  process.exit(8);
+};
+
+
+if (process.argv.length <= 2)
+  logErrorAndExit("Wrong usage");
+
+
+var common = require("../lib/common.js");
+var download = require("../lib/download.js");
+
+
+
+
+
+
+
+var input = common.getUserInput();
+console.log(input);
+
+
+download.download(input, function(err) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  download.unzip(input, function(err) {
+    if (err) {
+      console.log(err);
+      return;
+    } else {
+      console.log("unzipped!");
+    }
+  });
+});
