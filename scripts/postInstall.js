2c1364fc5c9f64fb08394a94ebfe5a2a519754e1
ktrzeciaknubisa <ktrzeciak@nubisa.com>
2015-07-31 23:02:14 +0200

    updates

--- scripts/postInstall.js
+++ scripts/postInstall.js
@@ -2,3 +2,24 @@
 
 // Copyright & License details are available under JXCORE_LICENSE file
 
+var fs = require("fs");
+var path = require("path");
+
+var common = require("../lib/common.js");
+var jx_utils = require("../lib/jx_utils.js");
+
+try {
+  if (!fs.existsSync(common.dirVersions))
+    fs.mkdirSync(common.dirVersions);
+} catch (ex) {
+  console.error("Cannot create local dir: " + common.dirVersions, "\n", ex);
+  process.exit();
+}
+
+try {
+  fs.chmod(common.dirVersions, 0777);
+} catch (ex) {
+  console.error("Cannot chmod local dir: " + common.dirVersions, "\n", ex);
+  process.exit();
+}
+
