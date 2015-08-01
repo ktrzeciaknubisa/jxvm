414bc8470078029228c173a5eadacfd8a55d2ebb
ktrzeciaknubisa <ktrzeciak@nubisa.com>
2015-08-01 19:53:11 +0200

    updates

--- scripts/preInstall.js
+++ scripts/preInstall.js
@@ -10,17 +10,20 @@ var common = require("../lib/common.js");
 var jx_utils = require("../lib/jx_utils.js");
 var console = jx_utils.console;
 
+
+console.log("### PREINSTALL");
+
 if (process.platform === "win32") {
 
 } else {
   var org = "/usr/local/bin/jx";
   var backup = "/usr/local/bin/jx_jxvm_backup";
   if (fs.existsSync(org)) {
-    cp.exec("mv " + org + " " + backup, function(err, stdout, stderr) {
+    //cp.exec("mv " + org + " " + backup, function(err, stdout, stderr) {
 
-      console.log(err, stdout + "", stderr + "");
+      //console.log(err, stdout + "", stderr + "");
 
-    });
+    //});
     //fs.writeFileSync(backup, fs.readFileSync(org));
     //fs.unlinkSync(org);
   }
